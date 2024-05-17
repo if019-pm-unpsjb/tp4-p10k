c7f9806a28cbe0a0a72a45dd937db208cddbfadf
Cristian <cristian-13.06@hotmail.com>
2024-05-17 18:08:25 -0300

    cliente-servidor-v1

--- server-tftp.c
+++ server-tftp.c
@@ -1,7 +1,240 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <errno.h>
 
-int main(int argc, char* argv[])
+#define PORT 69
+#define BUFFER_SIZE 516
+#define DATA_SIZE 512
+
+enum
 {
-    exit(EXIT_SUCCESS);
+    RRQ = 1,
+    WRQ,
+    DATA,
+    ACK,
+    ERROR
+};
+
+void error_exit(const char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void send_error(int sockfd, struct sockaddr_in *client_addr, socklen_t client_len, int error_code, const char *error_msg)
+{
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+
+    *(uint16_t *)buffer = htons(ERROR);
+    *(uint16_t *)(buffer + 2) = htons(error_code);
+    strcpy(buffer + 4, error_msg);
+
+    sendto(sockfd, buffer, 4 + strlen(error_msg) + 1, 0, (struct sockaddr *)client_addr, client_len);
+    printf("Enviado paquete ERROR código: %d, mensaje: %s\n", error_code, error_msg);
+}
+
+void handle_rrq(int sockfd, struct sockaddr_in *client_addr, socklen_t client_len, const char *filename)
+{
+    int file = open(filename, O_RDONLY);
+    if (file < 0)
+    {
+        if (errno == ENOENT)
+        {
+            send_error(sockfd, client_addr, client_len, 1, "File not found");
+        }
+        else if (errno == EACCES)
+        {
+            send_error(sockfd, client_addr, client_len, 2, "Access violation");
+        }
+        else
+        {
+            send_error(sockfd, client_addr, client_len, 0, "Not defined");
+        }
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
+    char data_buffer[DATA_SIZE];
+    int block = 1;
+    ssize_t read_bytes;
+
+    while ((read_bytes = read(file, data_buffer, DATA_SIZE)) > 0)
+    {
+        memset(buffer, 0, BUFFER_SIZE);
+        *(uint16_t *)buffer = htons(DATA);
+        *(uint16_t *)(buffer + 2) = htons(block);
+        memcpy(buffer + 4, data_buffer, read_bytes);
+
+        printf("Enviando paquete DATA número: %d\n", block);
+        sendto(sockfd, buffer, 4 + read_bytes, 0, (struct sockaddr *)client_addr, client_len);
+
+        // Esperar ACK
+        recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)client_addr, &client_len);
+        printf("Recibido paquete ACK número: %d\n", ntohs(*(uint16_t *)(buffer + 2)));
+
+        if (ntohs(*(uint16_t *)buffer) != ACK || ntohs(*(uint16_t *)(buffer + 2)) != block)
+        {
+            send_error(sockfd, client_addr, client_len, 4, "Illegal TFTP operation");
+            close(file);
+            return;
+        }
+
+        block++;
+    }
+
+    close(file);
+    printf("Envio concluido\n");
+}
+
+void handle_wrq(int sockfd, struct sockaddr_in *client_addr, socklen_t client_len, const char *filename)
+{
+    int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (file < 0)
+    {
+        if (errno == EEXIST)
+        {
+            send_error(sockfd, client_addr, client_len, 6, "File already exists");
+        }
+        else if (errno == EACCES)
+        {
+            send_error(sockfd, client_addr, client_len, 2, "Access violation");
+        }
+        else
+        {
+            send_error(sockfd, client_addr, client_len, 0, "Not defined");
+        }
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
+    int block = 0;
+
+    while (1)
+    {
+        // Enviar ACK
+        memset(buffer, 0, BUFFER_SIZE);
+        *(uint16_t *)buffer = htons(ACK);
+        *(uint16_t *)(buffer + 2) = htons(block);
+
+        printf("Enviando paquete ACK número: %d\n", block);
+        sendto(sockfd, buffer, 4, 0, (struct sockaddr *)client_addr, client_len);
+
+        // Recibir datos
+        ssize_t n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)client_addr, &client_len);
+        printf("Recibido paquete DATA número: %d\n", ntohs(*(uint16_t *)(buffer + 2)));
+        if (n < 0)
+        {
+            send_error(sockfd, client_addr, client_len, 0, "Undefined error");
+            close(file);
+            return;
+        }
+
+        int opcode = ntohs(*(uint16_t *)buffer);
+        int recv_block = ntohs(*(uint16_t *)(buffer + 2));
+
+        if (opcode == DATA && recv_block == block + 1)
+        {
+            if (write(file, buffer + 4, n - 4) < 0)
+            {
+                if (errno == ENOSPC)
+                {
+                    send_error(sockfd, client_addr, client_len, 3, "Disk full or allocation exceeded");
+                }
+                else
+                {
+                    send_error(sockfd, client_addr, client_len, 0, "Not defined");
+                }
+                close(file);
+                return;
+            }
+            block++;
+
+            if (n < BUFFER_SIZE)
+            {
+                // Enviar ultimo ACK
+                memset(buffer, 0, BUFFER_SIZE);
+                *(uint16_t *)buffer = htons(ACK);
+                *(uint16_t *)(buffer + 2) = htons(block);
+
+                printf("Enviando paquete ACK número: %d\n", block);
+                sendto(sockfd, buffer, 4, 0, (struct sockaddr *)client_addr, client_len);
+                break; // Fin de archivo
+            }
+        }
+        else
+        {
+            send_error(sockfd, client_addr, client_len, 4, "Illegal TFTP operation");
+            close(file);
+            return;
+        }
+    }
+
+    close(file);
+    printf("Fichero recibido.\n");
+}
+
+void handle_request(int sockfd, struct sockaddr_in *client_addr, socklen_t client_len)
+{
+    char buffer[BUFFER_SIZE];
+    ssize_t n;
+
+    n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)client_addr, &client_len);
+    if (n < 0)
+    {
+        error_exit("recvfrom");
+    }
+
+    int opcode = ntohs(*(uint16_t *)buffer);
+    char *filename = buffer + 2;
+
+    if (opcode == RRQ)
+    {
+        printf("Solicitud de lectura recibida para el archivo: %s\n", filename);
+        handle_rrq(sockfd, client_addr, client_len, filename);
+    }
+    else if (opcode == WRQ)
+    {
+        printf("Solicitud de escritura recibida para el archivo: %s\n", filename);
+        handle_wrq(sockfd, client_addr, client_len, filename);
+    }
+    else
+    {
+        send_error(sockfd, client_addr, client_len, 4, "Illegal TFTP operation");
+    }
+}
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+    {
+        error_exit("socket");
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        error_exit("bind");
+    }
+
+    while (1)
+    {
+        handle_request(sockfd, &client_addr, client_len);
+    }
+
+    close(sockfd);
+    return 0;
 }
---
+++ tftp_client.c
@@ -0,0 +1,209 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+#define SERVER_PORT 69
+#define BUFFER_SIZE 516
+#define DATA_SIZE 512
+
+enum
+{
+    RRQ = 1,
+    WRQ,
+    DATA,
+    ACK,
+    ERROR
+};
+
+void error_exit(const char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void send_rrq(int sockfd, struct sockaddr_in *server_addr, const char *filename)
+{
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+
+    *(uint16_t *)buffer = htons(RRQ);
+    strcpy(buffer + 2, filename);
+    strcpy(buffer + 2 + strlen(filename) + 1, "octet");
+
+    sendto(sockfd, buffer, 2 + strlen(filename) + 1 + strlen("octet") + 1, 0, (struct sockaddr *)server_addr, sizeof(*server_addr));
+    printf("Enviando solicitud de lectura para el archivo: %s\n", filename);
+
+    FILE *file = fopen(filename, "wb");
+    if (!file)
+    {
+        error_exit("fopen");
+    }
+
+    struct sockaddr_in from_addr;
+    socklen_t from_len = sizeof(from_addr);
+    ssize_t n;
+    int block = 1;
+
+    while (1)
+    {
+        n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&from_addr, &from_len);
+        int opcode = ntohs(*(uint16_t *)buffer);
+        int recv_block = ntohs(*(uint16_t *)(buffer + 2));
+
+        if (opcode == DATA && recv_block == block)
+        {
+            fwrite(buffer + 4, 1, n - 4, file);
+            printf("Recibido paquete DATA número: %d\n", recv_block);
+
+            memset(buffer, 0, BUFFER_SIZE);
+            *(uint16_t *)buffer = htons(ACK);
+            *(uint16_t *)(buffer + 2) = htons(block);
+
+            sendto(sockfd, buffer, 4, 0, (struct sockaddr *)&from_addr, from_len);
+            printf("Enviando paquete ACK número: %d\n", block);
+
+            if (n < BUFFER_SIZE)
+                break; // Fin de archivo
+            block++;
+        }
+        else if (opcode == ERROR)
+        {
+            fprintf(stderr, "Error recibido: %s\n", buffer + 4);
+            fclose(file);
+            return;
+        }
+        else
+        {
+            fprintf(stderr, "Error: operación TFTP ilegal\n");
+            fclose(file);
+            return;
+        }
+    }
+
+    fclose(file);
+    printf("Fichero recibido.\n");
+}
+
+void send_wrq(int sockfd, struct sockaddr_in *server_addr, const char *filename)
+{
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+
+    *(uint16_t *)buffer = htons(WRQ);
+    strcpy(buffer + 2, filename);
+    strcpy(buffer + 2 + strlen(filename) + 1, "octet");
+
+    sendto(sockfd, buffer, 2 + strlen(filename) + 1 + strlen("octet") + 1, 0, (struct sockaddr *)server_addr, sizeof(*server_addr));
+    printf("Enviando solicitud de escritura para el archivo: %s\n", filename);
+
+    FILE *file = fopen(filename, "rb");
+    if (!file)
+    {
+        error_exit("fopen");
+    }
+
+    struct sockaddr_in from_addr;
+    socklen_t from_len = sizeof(from_addr);
+    ssize_t n;
+    int block = 0;
+
+    while (1)
+    {
+        // Esperar ACK
+        n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&from_addr, &from_len);
+        printf("Recibido paquete ACK número: %d\n", ntohs(*(uint16_t *)(buffer + 2)));
+        if (n < 0)
+        {
+            error_exit("recvfrom");
+        }
+
+        int opcode = ntohs(*(uint16_t *)buffer);
+        int recv_block = ntohs(*(uint16_t *)(buffer + 2));
+
+        if (opcode == ACK && recv_block == block)
+        {
+            block++;
+
+            memset(buffer, 0, BUFFER_SIZE);
+            *(uint16_t *)buffer = htons(DATA);
+            *(uint16_t *)(buffer + 2) = htons(block);
+
+            n = fread(buffer + 4, 1, DATA_SIZE, file);
+            printf("Enviando paquete DATA número: %d\n", block);
+            sendto(sockfd, buffer, 4 + n, 0, (struct sockaddr *)&from_addr, from_len);
+
+            if (n < DATA_SIZE)
+            {
+                // Esperar ultimo ACK
+                n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&from_addr, &from_len);
+                printf("Recibido paquete ACK número: %d\n", ntohs(*(uint16_t *)(buffer + 2)));
+                break; // Fin de archivo
+            }
+        }
+        else if (opcode == ERROR)
+        {
+            fprintf(stderr, "Error recibido: %s\n", buffer + 4);
+            fclose(file);
+            return;
+        }
+        else
+        {
+            fprintf(stderr, "Error: operación TFTP ilegal\n");
+            fclose(file);
+            return;
+        }
+    }
+
+    fclose(file);
+    printf("Envio concluido\n");
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Uso: %s <IP servidor> <nombre de archivo> <r|w>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *server_ip = argv[1];
+    const char *filename = argv[2];
+    char mode = argv[3][0];
+
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+    {
+        error_exit("socket");
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0)
+    {
+        error_exit("inet_pton");
+    }
+
+    if (mode == 'r')
+    {
+        send_rrq(sockfd, &server_addr, filename);
+    }
+    else if (mode == 'w')
+    {
+        send_wrq(sockfd, &server_addr, filename);
+    }
+    else
+    {
+        fprintf(stderr, "Modo no válido. Use 'r' para lectura y 'w' para escritura.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    close(sockfd);
+    return 0;
+}
